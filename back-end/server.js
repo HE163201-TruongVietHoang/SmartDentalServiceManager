273fd2fe5241f442953105385ba252b01a8d1a47
kien23102003 <kiendthe170738@fpt.edu.vn>
2025-10-20 19:32:29 +0700

    update

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const db = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const authMiddleware = require("./middlewares/authMiddleware");
+const { getPool } = require("./config/db");
 
 const app = express();
 app.use(cors());
@@ -11,7 +12,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
-
+getPool();
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
