88aaa2790e718aa50f34af701b465179777d0686
kien23102003 <kiendthe170738@fpt.edu.vn>
2025-10-27 08:50:02 +0700

    okok

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -1,20 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { loginController,refreshTokenController,changePasswordController,requestPasswordResetController,resetPasswordController,registerController } = require('../controllers/authController');
-const { authMiddleware } = require('../middlewares/authMiddleware');
-router.post('/login', loginController);
-router.post('/refresh-token', refreshTokenController);
-router.post('/change-password', authMiddleware, changePasswordController);
-router.post('/request-reset-password', requestPasswordResetController);
-router.post('/reset-password', resetPasswordController);
-router.post('/register', registerController);
+const {
+  loginController,
+  refreshTokenController,
+  changePasswordController,
+  requestPasswordResetController,
+  resetPasswordController,
+  registerController,
+} = require("../controllers/authController");
+const { authMiddleware } = require("../middlewares/authMiddleware");
+router.post("/login", loginController);
+router.post("/refresh-token", refreshTokenController);
+router.post("/change-password", authMiddleware, changePasswordController);
+router.post("/request-reset-password", requestPasswordResetController);
+router.post("/reset-password", resetPasswordController);
+router.post("/register", registerController);
 
 // Account management (require authentication)
-const { listUsersController, getUserController, editUserController, updateRoleController, toggleUserActiveController, deleteUserController } = require('../controllers/authController');
-router.get('/users', listUsersController);
-router.get('/users/:id', getUserController);
-router.put('/users/:id', editUserController);
-router.patch('/users/:id/role', updateRoleController);
-router.patch('/users/:id/active', toggleUserActiveController);
-router.delete('/users/:id', deleteUserController);
+const {
+  listUsersController,
+  getUserController,
+  editUserController,
+  updateRoleController,
+  toggleUserActiveController,
+  deleteUserController,
+} = require("../controllers/authController");
+router.get("/users", listUsersController);
+router.get("/users/:id", getUserController);
+router.put("/users/:id", editUserController);
+router.patch("/users/:id/role", updateRoleController);
+router.patch("/users/:id/active", toggleUserActiveController);
+router.delete("/users/:id", deleteUserController);
 module.exports = router;
