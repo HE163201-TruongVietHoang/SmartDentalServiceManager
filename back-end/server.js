d3c7920966a3bda8cc03801663fe3c7079b11e17
NGUYEN VIET HA <vhntl20082003@gmail.com>
2025-11-04 17:30:46 +0700

    Promotion CRUD

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const db = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const authMiddleware = require("./middlewares/authMiddleware");
+const promotionRoutes = require('./routes/promotionRoutes');
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use('/api/promotions', promotionRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
