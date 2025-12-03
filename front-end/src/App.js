a33a5ba53ffec02e73b3c47ffaf473ecf2ab20c9
HungPTHE176412 <HungPTHE176412@fpt.edu.vn>
2025-12-03 09:59:52 +0000

    fix

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,13 +14,11 @@ import HomeDoctor from "./pages/HomeDoctor";
 // import DiagnosisPlan from "./components/doctor/DiagnosisPlan";
 // import TreatmentProgress from "./components/doctor/TreatmentProgress";
 // import ScheduleRequest from "./components/doctor/ScheduleRequest";
-
-import ProfilePage from "./pages/profile/ProfileView";
 import ServicesPage from "./pages/Hone/Service";
 import ServiceDetail from "./pages/Hone/ServiceDeatil";
 import DoctorsPage from "./pages/Hone/DoctorsPage";
 import DoctorDetail from "./pages/Hone/DoctorDetatil";
-
+import ProfilePage from "./pages/Profile/ProfileView";
 import ClinicManagerScheduleRequests from "./pages/ClinicManager/DoctorScheduleManager";
 
 import SignIn from "./pages/Account/SignIn";
